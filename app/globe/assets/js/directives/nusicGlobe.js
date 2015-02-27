8ccbc3730bc57cd8e8806a6bf61cd726a93a750d
Joseph Wolschon <jwolschon@payjunction.com>
2015-02-26 17:03:21 -0800

    web gl experimenting

--- app/globe/assets/js/directives/nusicGlobe.js
+++ app/globe/assets/js/directives/nusicGlobe.js
@@ -1,7 +1,34 @@
-angular.module('nusic.app.globe').directive('nusicGlobe', function () {
- return {
-     restrict: 'E',
-     templateUrl: 'globe/assets/partials/nusic-globe.html'
- }
+angular.module('nusic.app.globe').directive('nusicGlobe', function ($window) {
+    return {
+        restrict: 'E',
+        templateUrl: 'globe/assets/partials/nusic-globe.html',
+        controller: function () {
+
+        },
+        link: function (scope, element) {
+
+            var createEarth_ = function () {
+                var geometry = new THREE.SphereGeometry(0.5, 32, 32);
+                var material = new THREE.MeshBasicMaterial({
+                    color: 0x00ff00
+                });
+                var mesh = new THREE.Mesh(geometry, material);
+                return mesh
+            };
+
+            var scene = new THREE.Scene();
+            var camera = new THREE.PerspectiveCamera(45, $window.innerWidth / $window.innerHeight, 0.1, 1000);
+            camera.position.z = 2;
+
+            var renderer = new THREE.WebGLRenderer();
+            renderer.setSize($window.innerWidth, $window.innerHeight);
+            element.append(renderer.domElement);
+
+
+            scene.add(createEarth_());
+
+            renderer.render(scene, camera);
+        }
+    }
 
 });
